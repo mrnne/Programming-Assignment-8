9c08e23487756bc6be596096a3c7b7cbc59692be
mrnne <57556072+mrnne@users.noreply.github.com>
2019-11-30 05:39:00 +0800

    Add files via upload

--- Problem2 (Pandas).py
+++ Problem2 (Pandas).py
@@ -1,6 +1,6 @@
 import pandas as pd
 cars= pd.read_csv('cars.csv')
-carsA= cars.iloc[0:5,[0,2,4,6,8,10]]
+carsA= cars.iloc[0:5,[1,3,5,7,9,11]]
 carsB= cars.iloc[0]
 carsC= cars.loc[23,'cyl']
 carsD= cars.loc[[1,28,18],['Model','cyl','gear']]
